230fb9fe25f2de3f2b6aa42f4f81580475eb5879
njsaugat <njsaugat@gmail.com>
2024-01-02 16:55:53 +0545

    personalization

---
+++ app/dashboard/admin/info/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Info = () => {
+  return (
+    <div className="flex h-1/2 w-full items-center justify-center border-2 border-amber-300 text-5xl ">
+      Show Info
+    </div>
+  )
+}
+
+export default Info
