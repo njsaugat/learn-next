c2656a5c5259af94fa5aab8466879f513176d74b
njsaugat <njsaugat@gmail.com>
2024-01-02 16:55:34 +0545

    admin

---
+++ app/dashboard/admin/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div className="flex h-1/2 w-full items-center justify-center border-2 border-cyan-400 text-5xl ">
+      About
+    </div>
+  )
+}
+
+export default About
