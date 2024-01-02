f1086c7b06d1b21e308945949c8239eaa6906784
njsaugat <njsaugat@gmail.com>
2024-01-02 16:56:13 +0545

    personalization

---
+++ app/dashboard/admin/security/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Security = () => {
+  return (
+    <div className="flex h-1/2 w-full items-center justify-center border-2 border-violet-400 text-5xl ">
+      Security
+    </div>
+  )
+}
+
+export default Security
