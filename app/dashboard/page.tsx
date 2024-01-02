ec821f87fb012c59cd0519b8a63a2ebd7f29fc11
njsaugat <njsaugat@gmail.com>
2024-01-02 16:54:53 +0545

    page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Dashboard = () => {
+  return <div className="flex h-1/2 w-full items-center justify-center border-2 border-red-400 text-5xl ">Dashboard</div>
+}
+
+export default Dashboard
