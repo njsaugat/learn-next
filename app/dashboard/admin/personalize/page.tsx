3618b945ace9a1bdcb2622a95c0cb77f28498c1e
njsaugat <njsaugat@gmail.com>
2024-01-02 16:56:01 +0545

    personalization

---
+++ app/dashboard/admin/personalize/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Personalize = () => {
+  return (
+    <div className="flex h-1/2 w-full items-center justify-center border-2 border-indigo-400 text-5xl ">
+      Personalize
+    </div>
+  )
+}
+
+export default Personalize
