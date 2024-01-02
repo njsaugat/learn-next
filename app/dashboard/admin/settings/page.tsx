944212de12693f6eacb678f43e989e82beee4bdf
njsaugat <njsaugat@gmail.com>
2024-01-02 16:56:25 +0545

    import React from "react"

---
+++ app/dashboard/admin/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Settings = () => {
+  return (
+    <div className="flex h-1/2 w-full items-center justify-center border-2 border-blue-400 text-5xl ">
+      Settings
+    </div>
+  )
+}
+
+export default Settings
